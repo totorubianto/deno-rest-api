f70415ecbd37a07a7055638d8557af5af9203f43
Toto Rubianto <toto.rubianto.17@gmail.com>
2020-05-23 15:08:41 +0700

    add docker compose

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { App } from 'https://deno.land/x/alosaur/src/mod.ts';
 import { settings } from './app.settings.ts';
-
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 // create application
 const app = new App(settings);
+console.log(config({ safe: true }));
 app.listen();
