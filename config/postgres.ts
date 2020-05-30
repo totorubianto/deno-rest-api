ba8b91dc41e8048ed50a9c0641684d720445cfde
Toto Rubianto <toto.rubianto.17@gmail.com>
2020-05-31 06:42:47 +0700

    env

--- config/postgres.ts
+++ config/postgres.ts
@@ -3,20 +3,13 @@ import { Client } from "https://deno.land/x/postgres/mod.ts";
 export class Database {
     client: Client;
     constructor() {
-        // this.client = new Client({
-        //     database: "bookstore",
-        //     hostname: "postgres",
-        //     port: 5432,
-        //     user: "user",
-        //     password: "pass",
-        // });
         this.client = new Client({
-          database: "bookstore",
-          hostname: "localhost",
-          port: 5432,
-          user: "postgres",
-          password: "ulum1701",
-        });  
+            database: Deno.env.get('DB_NAME'),
+            hostname: Deno.env.get('DB_HOST'),
+            port: parseInt(Deno.env.get('DB_PORT') || "5000"),
+            user: Deno.env.get('DB_USER'),
+            password: Deno.env.get('DB_PASSWORD'),
+        });
     }
     async query(sql: string) {
         await this.client.connect();
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { App } from 'https://deno.land/x/alosaur/src/mod.ts';
 import { settings } from './app.settings.ts';
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 // create application
+config({ safe: true, export: true });
 const app = new App(settings);
-config({ safe: true });
 app.listen();
